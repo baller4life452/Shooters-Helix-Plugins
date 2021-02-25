df29740ffe0e4d66b567d4ebd6f61301a738b6aa
SHOOTER <43100788+baller4life452@users.noreply.github.com>
2021-02-25 09:59:46 -0600

    Update sh_tarot.lua

--- tarot/items/sh_tarot.lua
+++ tarot/items/sh_tarot.lua
@@ -1,5 +1,5 @@
 ITEM.name = "Tarot Deck"
-ITEM.description = "A deck of the Major Arcana, used for tarot readings."
+ITEM.description = "A deck of the Major Arcana, used for tarot readings. (/Tarot)"
 ITEM.price = 0
 ITEM.model = "models/props_lab/box01a.mdl"
 ITEM.skin = 1
