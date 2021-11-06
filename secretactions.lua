f19db84fe3cd2ce0f48f43cd291c236e89766e00
SHOOTER <43100788+baller4life452@users.noreply.github.com>
2021-11-06 18:40:46 -0500

    Update secretactions.lua
    Fixed SecretAction and used proper stuff

--- secretactions.lua
+++ secretactions.lua
@@ -11,9 +11,9 @@ function PLUGIN:InitializedChatClasses()
 			return Color(color.r - 35, color.g - 35, color.b - 35)
 		end,
 		CanHear = function(self, speaker, listener)
-            local trace = util.TraceLine{start = speaker:GetPos(), endpos = listener:GetPos()}
-            if !trace.HitWorld then
-                if !(speaker:GetPos():Distance(listener:GetPos()) >= ix.config.Get("chatRange", 280)) then
+            local trace = util.TraceLine{start = speaker:EyePos(), mask  = MASK_SOLID_BRUSHONLY, endpos = listener:EyePos()}
+            if !trace.Hit then
+                if (speaker:EyePos():Distance(listener:EyePos()) <= ix.config.Get("chatRange", 280)) then
                     return true
                 end
             end
